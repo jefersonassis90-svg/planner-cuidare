daff2c99b809abdbb33ded411980095d91b7fc84
jefersonassis90-svg <jeferson.assis90@gmail.com>
2026-07-31 14:11:56 -0300

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://edtlwgxgmomnggwbcfuu.supabase.co",
+  SUPABASE_URL: "https://edtlwgxgmomnqgwbcfuu.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_KgzykjyWtrtI-CpfBV76gg_PEc7DWwk"
 };
